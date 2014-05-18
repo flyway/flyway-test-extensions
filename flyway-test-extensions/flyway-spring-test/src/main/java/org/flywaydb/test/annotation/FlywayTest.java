3ac820257d15b9ac33206048df76cfff35497d57
FlorianGWE <florian.g.w.e4@gmail.com>
2014-05-18 17:04:59 +0200

    Issue #7change structure for flyway-spring-test

---
+++ flyway-test-extensions/flyway-spring-test/src/main/java/org/flywaydb/test/annotation/FlywayTest.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (C) 2011-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.test.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+/**
+ * Support for executing flyway commands during test without any special flyway command in test code.</p>
+ *
+ * The default behavior is to execute the flyway commands:
+ * <ul>
+ * <li>{@link org.flywaydb.core.Flyway#clean()}</li>
+ * <li>{@link org.flywaydb.core.Flyway#init()}</li>
+ * <li>{@link org.flywaydb.core.Flyway#migrate()}</li>
+ * </ul>
+ *
+ * For usage together with JUnit see {@link org.flywaydb.test.junit.FlywayTestExecutionListener}.</p>
+ *
+ * @author Florian
+ * @version  2011-12-10
+ * @version 1.7
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FlywayTest {
+
+	/**
+	 * invoke flyway command clean before a init/migrate call.</p>
+	 *
+	 *  Default: true
+	 */
+	public boolean invokeCleanDB() default true;
+
+	/**
+	 * invoke flyway command init before a migrate call</p>
+	 *
+	 *  Default: true
+	 */
+	public boolean invokeInitDB() default true;
+
+	/**
+	 * invoke flyway command migrate </p>
+	 *
+	 *  Default: true
+	 */
+	public boolean invokeMigrateDB() default true;
+
+	/**
+	 * Support to change the default setting for the base directory setting. </p>
+	 *
+	 * For each entry in the list a separate flyway migrate call will be executed.</br>
+	 * Afterwards the default base directory will be set.</p>
+	 *
+	 * Default: empty list - default settings will be used. <p/>
+	 *
+	 * Attention: This annotation are deprecated and can not used together  with {@link #locationsForMigrate}.
+	 *
+	 * @deprecated
+	 */
+	public String[] baseDirsForMigrate() default {};
+
+	/**
+	 * Support to add locations to the default location settings. <p/>
+	 *
+	 * if {link #overrideLocations()} returns true the complete locations will
+	 * be changed.<p/>
+	 *
+	 * Default: empty list  <p/>
+	 */
+	public String[] locationsForMigrate() default {};
+
+	/**
+	 * With this attribute the handling of the locationsForMigrate can be changed. <p/>
+	 *
+	 * Default: false
+	 */
+	public boolean overrideLocations() default false;
+}
---
+++ flyway-test-extensions/flyway-test-sample-spring256/src/test/java/org/flywaydb/test/sample/spring256/package-info.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2011-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Flyway test extension for unit testing.</p>
+ *
+ * A example project to show the integration of flyway-spring-test extension
+ * in a test with spring version 2.5.6.
+ *
+ * <ul>
+ * <li>Maven extension see the pom.xml file</li>
+ * <li>Usage inside a test see {@link org.flywaydb.test.sample.spring256.Spring256JUnitTest}
+ * <li>Test resources with SQL statements see the directories
+ * <b>loadmsql</b> and <b>sampletest256</b></li>
+ * </ul>
+ *
+ * Per default the project will execute all test again H2 database. </br>
+ * If you want to execute a test again a oracle database use Profile oracle-test:
+ * <code>
+ *   mvn clean install -P oracle-test
+ * </code>
+ *
+ *
+ * @version 1.0
+ * @version 2012-01-04
+ *
+ */
+package org.flywaydb.test.sample.spring256;
+
