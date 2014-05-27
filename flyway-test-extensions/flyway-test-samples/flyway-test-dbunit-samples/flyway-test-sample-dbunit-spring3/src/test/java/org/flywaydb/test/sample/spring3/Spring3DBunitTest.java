94323f3242096b57c217d0f5387185621656d6ac
FlorianGWE <florian.g.w.e4@gmail.com>
2014-05-27 23:47:26 +0200

    add abstraction level for sample projects

---
+++ flyway-test-extensions/flyway-test-samples/flyway-test-dbunit-samples/flyway-test-sample-dbunit-spring3/src/test/java/org/flywaydb/test/sample/spring3/Spring3DBunitTest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (C) 2011-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.test.sample.spring3;
+
+import org.flywaydb.test.annotation.FlywayTest;
+import org.flywaydb.test.dbunit.DBUnitSupport;
+import org.flywaydb.test.dbunit.FlywayDBUnitTestExecutionListener;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Simple Test to show how the annotation can be used inside test execution.
+ *
+ * @author florian
+ *
+ * @version 1.7
+ * @version 2012-01-06
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/context/simple_applicationContext.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		FlywayDBUnitTestExecutionListener.class })
+@FlywayTest
+@DBUnitSupport(loadFilesForRun = { "INSERT", "/dbunit3/dbunit.cus1.xml"})
+public class Spring3DBunitTest extends BaseDBHelper {
+
+	/**
+	 * Normal test method nothing done per startup
+	 */
+	@Test
+	public void dummyTestNoLoad() throws Exception {
+		int res = countCustomer();
+
+        assertTrue("This test must runs without an error, because we can not guarantee that this test method run as first. " + res, true);
+    }
+
+	/**
+	 * Made a clean init migrate usage before execution of test methods
+	 */
+	@Test
+	@FlywayTest
+	public void dummyTestMethodLoad() throws Exception {
+		int res = countCustomer();
+
+		assertEquals("Count of customer", 0, res);
+	}
+
+	/**
+	 * Made a clean init migrate and insert sql with dbunit
+	 */
+	@Test
+	@FlywayTest
+	@DBUnitSupport(loadFilesForRun = { "INSERT", "/dbunit3/dbunit.cus1.xml"})
+	public void loadDBUnitSQLs() throws Exception {
+		int res = countCustomer();
+
+		assertEquals("Count of customer", 2, res);
+	}
+
+	/**
+	 * Made a clean init migrate with inserts from flyway and store it
+	 * afterwards
+	 */
+	@Test
+	@FlywayTest(locationsForMigrate = {"loadmsql"})
+	@DBUnitSupport(saveTableAfterRun = { "CUSTOMER", "select * from CUSTOMER" }, saveFileAfterRun = "target/dbunitresult/customer1.xml")
+	public void storeDBUnitSQLs() throws Exception {
+		int res = countCustomer();
+
+		assertEquals("Count of customer", 2, res);
+	}
+
+}
---
+++ flyway-test-extensions/flyway-test-samples/flyway-test-spring-samples/flyway-test-sample-spring256/src/test/java/org/flywaydb/test/sample/spring256/Spring256JUnitTest.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2011-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.test.sample.spring256;
+
+import org.flywaydb.test.annotation.FlywayTest;
+import org.flywaydb.test.junit.FlywayTestExecutionListener;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Simple Test to show how the annotation can be used inside test execution.
+ *
+ * In this example it will be used spring version 2.5.6
+ *
+ * @author florian
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/context/simple_applicationContext_spring256.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		FlywayTestExecutionListener.class })
+@FlywayTest // Attention: this will be done nothing with spring 2.5.6
+public class Spring256JUnitTest extends BaseDBHelper {
+
+	/**
+	 * Normal test method nothing done per startup. With Spring 2.5.6 we
+	 * have no database setup during test class setup.
+	 */
+	@Test
+	public void dummyTestNoLoad() throws Exception {
+        int res = countCustomer();
+
+        assertTrue("This test must runs without an error, because we can not guarantee that this test method run as first. " + res, true);
+    }
+
+	/**
+	 * Made a clean init migrate usage before execution of test method.
+	 */
+	@Test
+	@FlywayTest
+	public void dummyTestMethodLoad() throws Exception {
+		int res = countCustomer();
+
+		assertEquals("Count of customer", 0, res);
+	}
+
+	/**
+	 * Made a clean init migrate usage before execution of test method.
+	 * It will be invoke two migration steps, one for <b>sampletest256</b> and than
+	 * <b>loadmsql</b>.
+	 */
+	@Test
+	@FlywayTest(locationsForMigrate = {"sampletest256", "loadmsql" })
+	public void loadMultibleSQLs() throws Exception {
+		int res = countCustomer();
+
+		assertEquals("Count of customer", 2, res);
+	}
+
+}
