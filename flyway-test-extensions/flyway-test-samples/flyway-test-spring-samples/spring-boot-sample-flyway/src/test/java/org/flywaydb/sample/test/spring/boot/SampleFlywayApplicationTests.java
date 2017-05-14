f9fc7ff156fa95171c44dd3c8ef9695a3da36ecf
FlorianGWE <florian.g.w.e4@gmail.com>
2017-05-14 22:28:18 +0200

    issue #30: ugrade to spring boot version 1.5.3 (fix).

--- flyway-test-extensions/flyway-test-samples/flyway-test-spring-samples/spring-boot-sample-flyway/src/test/java/org/flywaydb/sample/test/spring/boot/SampleFlywayApplicationTests.java
+++ flyway-test-extensions/flyway-test-samples/flyway-test-spring-samples/spring-boot-sample-flyway/src/test/java/org/flywaydb/sample/test/spring/boot/SampleFlywayApplicationTests.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2011-2017 the original author or authors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,14 +18,15 @@ package org.flywaydb.sample.test.spring.boot;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SampleFlywayApplication.class)
+@SpringBootTest(classes = SampleFlywayApplication.class)
 /**
  * Show standard spring-boot test test.
  *
@@ -33,13 +34,14 @@ import static org.junit.Assert.assertEquals;
  */
 public class SampleFlywayApplicationTests {
 
-	@Autowired
-	private JdbcTemplate template;
+    @Autowired
+    private JdbcTemplate template;
 
-	@Test
-	public void testDefaultSettings() throws Exception {
-		assertEquals(new Integer(3), this.template.queryForObject(
-				"SELECT COUNT(*) from PERSON", Integer.class));
-	}
+    @Test
+    public void testDefaultSettings() throws Exception {
+        assertThat(template.queryForObject(
+                "SELECT COUNT(*) from PERSON", Integer.class),
+                is(3));
+    }
 
 }
--- flyway-test-extensions/flyway-test-samples/flyway-test-spring-samples/spring-boot-sample-flyway/src/test/java/org/flywaydb/sample/test/spring/boot/flywaytest/FlywayTestApplicationTest.java
+++ flyway-test-extensions/flyway-test-samples/flyway-test-spring-samples/spring-boot-sample-flyway/src/test/java/org/flywaydb/sample/test/spring/boot/flywaytest/FlywayTestApplicationTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2011-2017 the original author or authors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,30 +15,30 @@
  */
 package org.flywaydb.sample.test.spring.boot.flywaytest;
 
+import org.flywaydb.sample.test.spring.boot.SampleFlywayApplication;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.junit.FlywayTestExecutionListener;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.flywaydb.sample.test.spring.boot.SampleFlywayApplication;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SampleFlywayApplication.class)
-@TransactionConfiguration
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, FlywayTestExecutionListener.class })
+@SpringBootTest(classes = SampleFlywayApplication.class)
+@Rollback
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, FlywayTestExecutionListener.class})
 @FlywayTest
 /**
  * Show a spring-boot test configuration with {@link FlywayTest} annotation.
  *
- * @since 3.2.1.1
  */
 public class FlywayTestApplicationTest {
 
@@ -48,15 +48,17 @@ public class FlywayTestApplicationTest {
     @FlywayTest(invokeCleanDB = true)
     @Test
     public void singleLocation() throws Exception {
-        assertEquals(new Integer(3), this.template.queryForObject(
-                "SELECT COUNT(*) from PERSON", Integer.class));
+        assertThat(template.queryForObject(
+                "SELECT COUNT(*) from PERSON", Integer.class),
+                is(3));
     }
 
     @FlywayTest(locationsForMigrate = {"/db/migration", "/db/migration2"})
     @Test
     public void twoLocations() throws Exception {
-        assertEquals(new Integer(5), this.template.queryForObject(
-                "SELECT COUNT(*) from PERSON", Integer.class));
+        assertThat(template.queryForObject(
+                "SELECT COUNT(*) from PERSON", Integer.class),
+                is(5));
     }
 
 }
