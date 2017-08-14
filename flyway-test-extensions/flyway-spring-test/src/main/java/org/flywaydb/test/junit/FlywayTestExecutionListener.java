flyway/flyway-test-extensions

commit 2f1607b13edf2f6311268b339d881bb8f87a91d0

Add FlywayTests annotation
New `FlywayTests` annotation is added as a container for multiple
`FlywayTest` annotations.

See gh-37

flyway-test-extensions/flyway-spring-test/src/main/java/org/flywaydb/test/junit/FlywayTestExecutionListener.java
@@ -20,17 +20,19 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.test.context.TestContext;
-import org.springframework.test.context.TestExecutionListener;
-
 import org.flywaydb.core.Flyway;
 import org.flywaydb.test.ExecutionListenerHelper;
 import org.flywaydb.test.annotation.FlywayTest;
+import org.flywaydb.test.annotation.FlywayTests;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListener;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 
 /**
@@ -108,6 +110,7 @@ import org.springframework.test.context.support.AbstractTestExecutionListener;
  * </p>
  *
  * @author Florian
+ * @author Eddú Meléndez
  *
  * @version 2011-12-10
  * @version 1.7
@@ -154,10 +157,10 @@ public class FlywayTestExecutionListener
 	public void beforeTestClass(final TestContext testContext) throws Exception {
 		// no we check for the DBResetForClass
 		final Class<?> testClass = testContext.getTestClass();
-
-		final Annotation annotation = AnnotationUtils.findAnnotation(testClass, FlywayTest.class);
-
-		dbResetWithAnotation(testContext, (FlywayTest) annotation);
+		Set<FlywayTest> annotations = AnnotatedElementUtils.findMergedRepeatableAnnotations(testClass, FlywayTest.class, FlywayTests.class);
+		for (FlywayTest annotation : annotations) {
+			dbResetWithAnnotation(testContext, annotation);
+		}
 	}
 
 	/**
@@ -203,7 +206,7 @@ public class FlywayTestExecutionListener
         if (firstFlywayTestAnnotation != null) {
             getLogger().debug("Method " + beforeMethod.getName() + " using flyway annotation " + firstFlywayTestAnnotation);
 
-            dbResetWithAnotation(testContext, (FlywayTest) firstFlywayTestAnnotation);
+            dbResetWithAnnotation(testContext, (FlywayTest) firstFlywayTestAnnotation);
         }
 
     }
@@ -221,10 +224,10 @@ public class FlywayTestExecutionListener
 			throws Exception {
 		final Method testMethod = testContext.getTestMethod();
 
-		final Annotation annotation = testMethod
-				.getAnnotation(FlywayTest.class);
-
-		dbResetWithAnotation(testContext, (FlywayTest) annotation);
+		Set<FlywayTest> annotations = AnnotatedElementUtils.findMergedRepeatableAnnotations(testMethod, FlywayTest.class, FlywayTests.class);
+		for (FlywayTest annotation : annotations) {
+			dbResetWithAnnotation(testContext, annotation);
+		}
 	}
 
 	/**
@@ -259,7 +262,7 @@ public class FlywayTestExecutionListener
 	 * @param annotation
 	 *            founded
 	 */
-	private void dbResetWithAnotation(final TestContext testContext,
+	private void dbResetWithAnnotation(final TestContext testContext,
 			final FlywayTest annotation) {
 		if (annotation != null) {
 			Flyway flyWay = null;

flyway-test-extensions/flyway-spring-test/pom.xml
@@ -89,7 +89,15 @@
                                 <flyway.url>jdbc:h2:nio:${project.build.directory}/db/flywaytest.db</flyway.url>
                                 <flyway.user>h2_test</flyway.user>
                                 <flyway.password>h2_test</flyway.password>
-
+								<jdbc2.driver>org.h2.Driver</jdbc2.driver>
+								<jdbc2.url>jdbc:h2:nio:${project.build.directory}/db/flywaytest2.db</jdbc2.url>
+								<jdbc2.username>h2_test</jdbc2.username>
+								<jdbc2.password>h2_test</jdbc2.password>
+								<flyway2.locations>/basetest2</flyway2.locations>
+								<flyway2.driver>org.h2.Driver</flyway2.driver>
+								<flyway2.url>jdbc:h2:nio:${project.build.directory}/db/flywaytest2.db</flyway2.url>
+								<flyway2.user>h2_test</flyway2.user>
+								<flyway2.password>h2_test</flyway2.password>
                             </systemPropertyVariables>
 							<includes>
 								<include>**/*JUnitTest.java</include>

flyway-test-extensions/flyway-spring-test/src/main/java/org/flywaydb/test/annotation/FlywayTest.java
@@ -15,6 +15,7 @@
  */
 package org.flywaydb.test.annotation;
 
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -36,6 +37,7 @@ import java.lang.annotation.RetentionPolicy;
  * @version 1.7
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(FlywayTests.class)
 public @interface FlywayTest {
 
 	/**

flyway-test-extensions/flyway-spring-test/src/main/java/org/flywaydb/test/annotation/FlywayTests.java
@@ -1,0 +1,16 @@
+package org.flywaydb.test.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author Eddú Meléndez
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FlywayTests {
+
+	FlywayTest[] value();
+
+}

flyway-test-extensions/flyway-spring-test/src/main/resources/context/secondFlywayContainerContext.xml
@@ -1,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+
+    Copyright (C) 2011-2017 the original author or authors.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+-->
+<beans xmlns="http://www.springframework.org/schema/beans"
+       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+       xmlns:context="http://www.springframework.org/schema/context"
+       xsi:schemaLocation="http://www.springframework.org/schema/beans
+        http://www.springframework.org/schema/beans/spring-beans-3.0.xsd
+        http://www.springframework.org/schema/context
+        http://www.springframework.org/schema/context/spring-context-3.0.xsd">
+
+    <!--  Needed for system properties replacement  -->
+    <!-- Following will only work with spring 3 	-->
+    <context:property-placeholder location="classpath*:flyway2.properties"
+                                  ignore-resource-not-found="true"
+                                  system-properties-mode="OVERRIDE"/>
+
+    <!-- import the data source bean -->
+    <bean id="dataSourceRef2" name="dataSource2"
+          class="org.apache.commons.dbcp.BasicDataSource" destroy-method="close">
+        <property name="driverClassName" value="${flyway2.driver}"/>
+        <property name="url" value="${flyway2.url}"/>
+        <property name="username" value="${flyway2.user}"/>
+        <property name="password" value="${flyway2.password}"/>
+        <property name="maxActive" value="-1"/>
+
+    </bean>
+
+    <!-- import the flyway creation bean -->
+    <bean id="flywayFactory2" class="org.flywaydb.test.junit.FlywayHelperFactory" depends-on="dataSourceRef2"/>
+
+    <bean id="flyway2" class="org.flywaydb.core.Flyway" depends-on="dataSourceRef2" factory-bean="flywayFactory2" factory-method="createFlyway">
+        <property name="dataSource" ref="dataSourceRef2"/>
+        <property name="locations" value="${flyway2.locations}"/>
+    </bean>
+
+</beans>

flyway-test-extensions/flyway-spring-test/src/test/java/org/flywaydb/test/sample/junit/BaseJUnitFlywayContextTests.java
@@ -1,0 +1,132 @@
+/**
+ * Copyright (C) 2011-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.flywaydb.test.sample.junit;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.test.annotation.FlywayTest;
+import org.flywaydb.test.annotation.FlywayTests;
+import org.flywaydb.test.junit.FlywayTestExecutionListener;
+import org.flywaydb.test.sample.helper.BaseDBHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Simple Test to show how the annotation can be used inside test execution and use a
+ * flywayContainerContext.
+ *
+ * @author Eddú Meléndez
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = { "/context/flywayContainerContext.xml", "/context/secondFlywayContainerContext.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		FlywayTestExecutionListener.class })
+@FlywayTests(value = {
+		@FlywayTest(flywayName = "flyway"),
+		@FlywayTest(flywayName = "flyway2")
+})
+public class BaseJUnitFlywayContextTests extends BaseDBHelper {
+
+	/**
+	 * Normal test method nothing done per startup
+	 */
+	@Test
+	public void dummyTestNoLoad() throws Exception {
+		int res = countCustomer();
+		int res2 = countCustomer2();
+
+        assertTrue("This test must runs without an error, because we can not guarantee that this test method run as first. " + res, true);
+        assertTrue("This test must runs without an error, because we can not guarantee that this test method run as first. " + res2, true);
+    }
+
+	/**
+	 * Made a clean init migrate usage before execution of test methods
+	 */
+	@Test
+	@FlywayTests(value = {
+			@FlywayTest(flywayName = "flyway"),
+			@FlywayTest(flywayName = "flyway2")
+	})
+	public void dummyTestMethodLoad() throws Exception {
+		int res = countCustomer();
+		int res2 = countCustomer2();
+
+		assertEquals("Count of customer", 0, res);
+		assertEquals("Count of customer2", 0, res2);
+	}
+
+	/**
+	 * Made a clean init migrate usage before execution of test methods
+	 */
+	@Test
+	@FlywayTests(value = {
+			@FlywayTest(locationsForMigrate = {"basetest", "loadMultibleSQLs"}, overrideLocations = true, flywayName = "flyway"),
+			@FlywayTest(locationsForMigrate = {"basetest2", "loadMultipleSQLs2"}, overrideLocations = true, flywayName = "flyway2")
+	})
+	public void loadMultipleSQLsOverrideLocations() throws Exception {
+		int res = countCustomer();
+		int res2 = countCustomer2();
+
+		assertEquals("Count of customer", 2, res);
+		assertEquals("Count of customer2", 2, res2);
+	}
+
+	/**
+	 * Made a clean init migrate usage before execution of test methods
+	 */
+	@Test
+	@FlywayTests(value = {
+			@FlywayTest(locationsForMigrate = { "loadMultibleSQLs" }, flywayName = "flyway"),
+			@FlywayTest(locationsForMigrate = { "loadMultipleSQLs2" }, flywayName = "flyway2")
+	})
+	public void loadMultipleSQLsLocations() throws Exception {
+		int res = countCustomer();
+		int res2 = countCustomer2();
+
+		assertEquals("Count of customer", 2, res);
+		assertEquals("Count of customer2", 2, res2);
+	}
+
+	private int countCustomer2() throws Exception {
+		DataSource ds = (DataSource) context.getBean("dataSourceRef2");
+		int result = -1;
+
+		Statement stmt = ds.getConnection().createStatement();
+		String query = "select count(*) from Customer2";
+
+		ResultSet rs = stmt.executeQuery(query);
+		rs.next();
+		Long cnt = rs.getLong(1);
+		result = cnt.intValue();
+
+		rs.close();
+		stmt.close();
+
+		return result;
+	}
+
+}

flyway-test-extensions/flyway-spring-test/src/test/resources/basetest2/V1.0.1__Base_Version.sql
@@ -1,0 +1,33 @@
+--
+-- Copyright (C) 2011-2017 the original author or authors.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--         http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+/*
+ * Base test for h2 tests
+*/
+
+
+
+CREATE TABLE CUSTOMER2 (
+    cus_id NUMBER(38,0)  CONSTRAINT cus_ID_NN NOT NULL,
+    cus_name VARCHAR(50)  CONSTRAINT cus_NAME_NN NOT NULL
+);
+
+CREATE INDEX CUS_CUS_ID_I ON CUSTOMER2(CUS_ID) ;
+ALTER TABLE CUSTOMER2 ADD CONSTRAINT CUS_ID_PK PRiMARY KEY (CUS_ID);
+ALTER TABLE CUSTOMER2 ADD CONSTRAINT CUS_NAME_UK UNIQUE (CUS_NAME);
+
+CREATE SEQUENCE CUS_SEQ START WITH 1000 INCREMENT by 10;
+

commit cc85a5421a7901b2ca072c4282bbda45111c36f2

Fix compatibility with old spring versions
This commit adds backwards compatibility with different versions of spring
framework. Fix compilation issues introduced in 2f1607b.

See gh-37

flyway-test-extensions/flyway-spring-test/src/main/java/org/flywaydb/test/junit/FlywayTestExecutionListener.java
@@ -20,7 +20,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -30,7 +29,7 @@ import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.annotation.FlywayTests;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
@@ -157,8 +156,14 @@ public class FlywayTestExecutionListener
 	public void beforeTestClass(final TestContext testContext) throws Exception {
 		// no we check for the DBResetForClass
 		final Class<?> testClass = testContext.getTestClass();
-		Set<FlywayTest> annotations = AnnotatedElementUtils.findMergedRepeatableAnnotations(testClass, FlywayTest.class, FlywayTests.class);
-		for (FlywayTest annotation : annotations) {
+		FlywayTests containerAnnotation = AnnotationUtils.getAnnotation(testClass, FlywayTests.class);
+		if (containerAnnotation != null) {
+			FlywayTest[] annotations = containerAnnotation.value();
+			for (FlywayTest annotation : annotations) {
+				dbResetWithAnnotation(testContext, annotation);
+			}
+		} else {
+			FlywayTest annotation = AnnotationUtils.getAnnotation(testClass, FlywayTest.class);
 			dbResetWithAnnotation(testContext, annotation);
 		}
 	}
@@ -224,8 +229,14 @@ public class FlywayTestExecutionListener
 			throws Exception {
 		final Method testMethod = testContext.getTestMethod();
 
-		Set<FlywayTest> annotations = AnnotatedElementUtils.findMergedRepeatableAnnotations(testMethod, FlywayTest.class, FlywayTests.class);
-		for (FlywayTest annotation : annotations) {
+		FlywayTests containerAnnotation = AnnotationUtils.getAnnotation(testMethod, FlywayTests.class);
+		if (containerAnnotation != null) {
+			FlywayTest[] annotations = containerAnnotation.value();
+			for (FlywayTest annotation : annotations) {
+				dbResetWithAnnotation(testContext, annotation);
+			}
+		} else {
+			FlywayTest annotation = AnnotationUtils.getAnnotation(testMethod, FlywayTest.class);
 			dbResetWithAnnotation(testContext, annotation);
 		}
 	}

flyway-test-extensions/flyway-spring-test/src/test/java/org/flywaydb/test/sample/junit/BaseJUnitFlywaysContextTest.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import static org.junit.Assert.assertEquals;
@@ -41,7 +41,7 @@ import static org.junit.Assert.assertTrue;
  *
  * @author Eddú Meléndez
  */
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/context/flywayContainerContext.xml", "/context/secondFlywayContainerContext.xml" })
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
 		FlywayTestExecutionListener.class })
@@ -49,7 +49,7 @@ import static org.junit.Assert.assertTrue;
 		@FlywayTest(flywayName = "flyway"),
 		@FlywayTest(flywayName = "flyway2")
 })
-public class BaseJUnitFlywayContextTests extends BaseDBHelper {
+public class BaseJUnitFlywaysContextTest extends BaseDBHelper {
 
 	/**
 	 * Normal test method nothing done per startup